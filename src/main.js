bccba0e0c1c74b700da78786b1d2b6357d3f0f5e
JuanDiegoTerriquezPolanco <jterriquez0@ucol.mx>
2020-12-10 20:05:00 -0600

    implemented contarImparesFor

--- src/main.js
+++ src/main.js
@@ -8,7 +8,17 @@ export default class App {
         }
         return aux;
     }
+    contarImparesFor(inicio,fin){
+        let aux=0;
+        for (let i = inicio; i <= fin; i++) {
+            if (i%2==0) {
+                aux++;
+            }    
+        }
+        return aux;
+    }
 }
 
 let app = new App();
 console.log(app.sumarParesFor());
+console.log(app.contarImparesFor(10,30));
