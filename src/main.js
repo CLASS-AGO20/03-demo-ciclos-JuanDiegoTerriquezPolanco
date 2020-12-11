964940bab13a2d379f6a97f447cf2b95b8ee2c62
JuanDiegoTerriquezPolanco <jterriquez0@ucol.mx>
2020-12-10 19:54:18 -0600

    implemented sumarParesFor

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
-export default class App {}
+export default class App {
+    sumarParesFor(){
+        let aux=0;
+        for (let i = 0; i <= 20; i++) {
+            if(i%2==0){
+                aux=aux+i;
+            }
+        }
+        return aux;
+    }
+}
 
 let app = new App();
+console.log(app.sumarParesFor());
